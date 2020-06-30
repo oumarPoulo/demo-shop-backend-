8d7851a33090aaef55045cf2d578a7a41cdaff13
Alpha Diallo <aob.diallo@gmail.com>
2020-06-30 17:54:35 +0200

    initialize project

---
+++ src/test/java/com/oumarpoulo/shopapp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oumarpoulo.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
